20fdbcbffe2a23efe4eae0fb1bcbb49acae70c70
SpySparsh <153936815+SpySparsh@users.noreply.github.com>
2025-11-12 17:24:22 +0530

    Update counter.jsx

--- client/src/counter.jsx
+++ client/src/counter.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
+const socket = io('https://stock-screener-b6t5.onrender.com');
 
 function App() {
   const [trades, setTrades] = useState([]);
